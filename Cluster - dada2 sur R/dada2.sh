8287757bea2e005c382516db06d719ffa03f8d07
HansSCHR <34349345+HansSCHR@users.noreply.github.com>
2019-03-13 11:55:18 +0100

    Script Bash for dada2 on R
    Allows to create a dada2 functional environment if the user want it and run the R dada2 script. 
    If you have ever installer a dada2 environment, the script just runs the script.

---
+++ Cluster - dada2 sur R/dada2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SCHRIEKE Hans 
+#Dada2 on R 
+
+
+module load system/conda/5.1.0
+echo "conda loaded"
+
+
+read -p "Do you want to install a dada2 environment with conda ? [y/n] " -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]
+then  
+	# dada2 environment installation + run the script
+	conda create -n dada2 zlib=1.2.8
+	source activate dada2
+	source /homedir/schrieke/anaconda/bin/activate dada3
+	conda install -c r r-base=3.5
+	conda install gxx_linux-64
+	conda install -c bioconda -c conda-forge bioconductor-shortread
+	conda install libiconv
+	Rscript dada2.R
+
+else [[ $REPLY =~ ^[Nn]$ ]]
+	# Just run the script
+	source activate dada2
+	Rscript dada2.R 
+fi
