bfa5fb2f70b008838a551d06661e1c5ccc12e818
Hans Schrieke <34349345+HansSCHR@users.noreply.github.com>
2019-03-25 16:41:36 +0100

    Import data, alpha div, NMDS
    Functionnal script to import dada2 outputs and use it with phyloseq

--- dada2 on R - without user interactions/phyloseq.R
+++ dada2 on R - without user interactions/phyloseq.R
@@ -5,17 +5,29 @@
 path <- "/gs7k1/home/schrieke/Fastq/tables"
 setwd(path)
 
-# Data import : OTU table, taxa table and data frame (metadata)
-OTU <- read.csv("seqtab.csv", sep=";", dec=",")
-taxa <- read.csv("taxa.csv", sep=";", dec=",")
-metada <- read.csv("metadata.csv", sep=";", dec=",")
 
-OTU_table <- as.matrix(OTU)
-taxa_table <- as.matrix(taxa)
+# Data import : OTU table, taxa table and data frame (metadata)
+otu <- read.csv("seqtabnochim.csv", sep=";", dec=",")
+taxa <- read.csv("taxa.csv", sep=";", dec=",")
+metadata <- read.csv("metadata_run1.csv", sep=",", row.names = 1)
+
+metadata[metadata=="N"] <- NA # negative controls 
+
+otu <- as.matrix(otu)
+#print("otu table (matrix) : ")
+#head(otu)
+
+taxa <- as.matrix(taxa)
+#print("taxa table (matrix) : ")
+#head(taxa)
+
+metadata <- as.data.frame(metadata)
+#print ("metadata (data frame) : ")
+#head(metadata)
 
 # Phyloseq installation
-source('http://bioconductor.org/biocLite.R')
-biocLite('phyloseq')
+#source('http://bioconductor.org/biocLite.R')
+#biocLite('phyloseq')
 
 # Phyloseq and ggplot2 loading 
 library(phyloseq); packageVersion("phyloseq")
@@ -23,6 +35,36 @@ library(ggplot2); packageVersion("ggplot2")
 
 theme_set(theme_bw()) #set ggplot2 graphic theme 
 
-ps <- phyloseq(otu_table(OTU_table.nochim, taxa_are_rows=FALSE), 
-               sample_data(metada), 
-               tax_table(taxa_table))
+# Phyloseq object creation
+OTU = otu_table(otu, taxa_are_rows = TRUE)
+TAX = tax_table(taxa)
+SAM = sample_data(metadata)
+
+ps <- phyloseq(OTU, TAX, SAM) 
+
+
+# Alpha diversity plot 
+dir.create("phyloseq_plot")
+path2 <- "/gs7k1/home/schrieke/Fastq/tables/phyloseq_plot"
+setwd(path2)
+
+pdf("5-alpha_diversity.pdf")
+plot_richness(ps, measures=c("Shannon", "Simpson"), color="LOCATION")
+dev.off()
+
+
+# Bray-Curtis NMDS
+ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
+ord.nmds.bray <- ordinate(ps.prop, method="NMDS", distance="bray")
+
+pdf("6-NMDS.pdf")
+plot_ordination(ps.prop, ord.nmds.bray, color="LOCATION", title="Bray NMDS")
+dev.off()
+
+setwd(path) 
+
+# Save the session
+install.packages("session", repos = "http://cran.us.r-project.org")
+library(session)
+save.session("phyloseq.Rda")
+
