a66839f35e31d9b03a2425aa4f27e0183b58cab3
Hans Schrieke <34349345+HansSCHR@users.noreply.github.com>
2019-04-02 14:00:47 +0200

    Update : plots correction
    Plots didn't work with function, I changed it

--- dada2 on R - without user interactions/dada2-v2.R
+++ dada2 on R - without user interactions/dada2-v2.R
@@ -8,36 +8,12 @@ library(dada2); packageVersion("dada2")
 
 ### FUNCTIONS ### 
 
-plot <- function(path, name, plotype, x, y){
+plot_folder <- function(path, name, plotype, x, y){
   folder <- sprintf("%s", name)
   dir.create(folder)
   path2 <- getwd()
   path3 <- file.path(path2, folder)
   setwd(path3)
-  
-  if (plotype == "quality"){
-    
-    pdf("1-plotForward.pdf")
-    plotQualityProfile(x, aggregate = TRUE) #forward reads
-    dev.off()
-    
-    pdf("2-plotReverse.pdf")
-    plotQualityProfile(y, aggregate = TRUE) #reverse reads
-    dev.off()
-  }
-  
-  if (plotype == "error"){
-    pdf("3-plotErrorsFwd.pdf")
-    plotErrors(x, nominalQ=TRUE)
-    dev.off()
-    
-    pdf("4-plotErrorsRvs.pdf")
-    plotErrors(y, nominalQ=TRUE)
-    dev.off()
-  }
-  
-  setwd(path)
-}
 
 
 dada2 <- function(path, run){
@@ -60,13 +36,11 @@ dada2 <- function(path, run){
 
   # Store forward and reverse reads 
   forward <- sort(list.files(path, pattern="_R1_001.fastq.gz", full.names = TRUE))
-  print(forward)
+  fwd <<- forward
   reverse <- sort(list.files(path, pattern="_R2_001.fastq.gz", full.names = TRUE))
-  print(reverse)
+  rvs <<- reverse
   # Store sample names 
   sample.names <- sapply(strsplit(basename(forward), "_"), `[`, 1) 
-  
-  plot(path,"plot-quality", "quality", forward, reverse)
 
   filtForward <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
   filtReverse <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
@@ -77,11 +51,10 @@ dada2 <- function(path, run){
   head(out)
 
   errForward <- learnErrors(filtForward, multithread=TRUE)
+  errFwd <<- errForward
   errReverse <- learnErrors(filtReverse, multithread=TRUE)
+  errRvs <<- errReverse
   
-  
-  plot (path, "plot-error", "error", errForward, errReverse)
-
   # Dereplication
   derepForward <- derepFastq(filtForward, verbose=TRUE)
   derepReverse <- derepFastq(filtReverse, verbose=TRUE)
@@ -139,14 +112,106 @@ dada2 <- function(path, run){
 path = "D:/data"
 setwd(path)
 
+## RUN 1 ##
+
+# DADA2
 dada2(path, "run1")
+ 
+# PLOTS
+plot_folder(path, "plot-quality")
+pdf("1-plotForward.pdf")
+plotQualityProfile(fwd, aggregate = TRUE) #forward reads
+dev.off()
+pdf("2-plotReverse.pdf")
+plotQualityProfile(rvs, aggregate = TRUE) #reverse reads
+dev.off()
+plot_folder(path, "plot-error")
+pdf("3-plotErrorsFwd.pdf")
+plotErrors(errFwd, nominalQ=TRUE)
+dev.off()
+pdf("4-plotErrorsRvs.pdf")
+plotErrors(errRvs, nominalQ=TRUE)
+dev.off()
+  
+path = "D:/data"
+setwd(path)
+  
+
+## RUN 2 ## 
+  
+# DADA2 #
 dada2(path, "run2")
+  
+# PLOTS
+plot_folder(path, "plot-quality")
+pdf("1-plotForward.pdf")
+plotQualityProfile(fwd, aggregate = TRUE) #forward reads
+dev.off()
+pdf("2-plotReverse.pdf")
+plotQualityProfile(rvs, aggregate = TRUE) #reverse reads
+dev.off()
+plot_folder(path, "plot-error")
+pdf("3-plotErrorsFwd.pdf")
+plotErrors(errFwd, nominalQ=TRUE)
+dev.off()
+pdf("4-plotErrorsRvs.pdf")
+plotErrors(errRvs, nominalQ=TRUE)
+dev.off()
+
+path = "D:/data"
+setwd(path)
+  
+
+## RUN 3 ## 
+
+# DADA2 
 dada2(path, "run3")
+
+# PLOTS
+plot_folder(path, "plot-quality")
+pdf("1-plotForward.pdf")
+plotQualityProfile(fwd, aggregate = TRUE) #forward reads
+dev.off()
+pdf("2-plotReverse.pdf")
+plotQualityProfile(rvs, aggregate = TRUE) #reverse reads
+dev.off()
+plot_folder(path, "plot-error")
+pdf("3-plotErrorsFwd.pdf")
+plotErrors(errFwd, nominalQ=TRUE)
+dev.off()
+pdf("4-plotErrorsRvs.pdf")
+plotErrors(errRvs, nominalQ=TRUE)
+dev.off()
+
+path = "D:/data"
+setwd(path)
+
+
+## All RUNS ## 
+  
+# DADA2 
 dada2(path, "runs")
+
+# PLOTS
+plot_folder(path, "plot-quality")
+pdf("1-plotForward.pdf")
+plotQualityProfile(fwd, aggregate = TRUE) #forward reads
+dev.off()
+pdf("2-plotReverse.pdf")
+plotQualityProfile(rvs, aggregate = TRUE) #reverse reads
+dev.off()
+plot_folder(path, "plot-error")
+pdf("3-plotErrorsFwd.pdf")
+plotErrors(errFwd, nominalQ=TRUE)
+dev.off()
+pdf("4-plotErrorsRvs.pdf")
+plotErrors(errRvs, nominalQ=TRUE)
+dev.off()
   
+path = "D:/data"
+setwd(path)
   
-  
-  
-  
-  
-  
+ 
+## SAVE SESSION ##   
+library(session); packageVersion("session")
+save.session("dada2_v2.Rda")
