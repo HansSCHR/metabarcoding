9efc0921f1ed6e9521e2f126b0d3e10466c0ba51
Hans Schrieke <34349345+HansSCHR@users.noreply.github.com>
2019-07-19 10:12:22 +0200

    update assign_taxonomy.sh

--- script_v3/4.Oligotyping/assignTaxonomy/qiime2/assign_taxonomy.sh
+++ script_v3/4.Oligotyping/assignTaxonomy/qiime2/assign_taxonomy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#$ -q normal.q
-#$ -N assign_taxo
+#$ -q long.q
+#$ -N final_assign_taxo
 #$ -M hans.schrieke@cirad.fr
 #$ -pe parallel_smp 15
 #$ -l mem_free=6G
@@ -12,33 +12,100 @@
 . /etc/profile.d/modules.sh
 
 module purge
-module load system/conda/5.1.0
+module load system/conda/4.4.10
 source activate qiime2-2019.4
 #conda list
 
-TMP_PATH=$"/homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers"
+TMP_PATH=$"/homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good"
 
 echo "TMP_PATH : " $TMP_PATH
+cd $TMP_PATH
+
+
+
 
 # Import data to qiime2 artefact
 
-qiime tools import \--type 'FeatureData[Sequence]' \--input-path S18_S1_L001_MERGED_uppercase.fasta \--output-path merged.qza
+for fasta in *.fasta
+do
+  fullfilename=$(basename $fasta)
+  sample=$(echo $fullfilename | cut -d'_' -f1)
+  qiime tools import \
+    --type 'FeatureData[Sequence]' \
+    --input-path $fasta \
+    --output-path merged_$sample.qza
+  #echo $sample " is succesfully imported"
+done
 
 echo "import data to qiime2 artefact : DONE"
 
+
+
+
 # Assign Taxonomy to ASV
 
-qiime feature-classifier classify-sklearn \
-  --i-classifier /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/silva-132-99-nb-classifier.qza \
-  --i-reads /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/merged.qza  \
-  --o-classification /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/taxonomy.qza \
+#mkdir taxonomy
+
+for qza in *.qza
+do
+  fullfilename=$(basename $qza)
+  sample=$(echo $fullfilename | cut -d'_' -f2 | cut -d'.' -f1)
+  qiime feature-classifier classify-sklearn \
+    --i-classifier /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/silva-132-99-nb-classifier.qza \
+    --i-reads /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/$qza  \
+    --o-classification /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/taxonomy_$sample.qza \
+  #echo $sample " is succesfully assigned"
+done
 
 echo "assign taxonomy : DONE"
 
-qiime metadata tabulate \
-  --m-input-file /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/taxonomy.qza  \
-  --o-visualization /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/taxonomy.qzv
 
-echo "create visualization file : DONE"
 
-source deactivate 
+
+# Export fasta from qza
+
+cd $TMP_PATH/taxonomy
+
+mkdir sequence_output
+
+for qza in *.qza
+do 
+  fullfilename=$(basename $qza)
+  sample=$(echo $fullfilename | cut -d'_' -f2 | cut -d'.' -f1)
+    qiime tools export \
+        --input-path /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/$qza \
+        --output-path /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/sequence_output/taxonomy_$sample
+    cd /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/sequence_output/taxonomy_$sample
+    mv *.tsv taxonomy_$sample.tsv
+    cd $TMP_PATH/taxonomy
+    echo $qza " is now exported"
+done
+
+echo "fasta export : DONE" 
+
+
+
+
+
+# Create visualization files 
+
+#cd $TMP_PATH/taxonomy
+
+for qza in *.qza
+do
+  fullfilename=$(basename $qza)
+  sample=$(echo $fullfilename | cut -d'_' -f2 | cut -d'.' -f1)
+  qiime metadata tabulate \
+    --m-input-file /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/$qza \
+    --o-visualization /homedir/schrieke/stage/oligotyping/assignement/qiime/training-feature-classifiers/sequence_merged_format_good/taxonomy/taxonomy_$sample.qzv
+  #echo $sample " is now available to watch"
+done
+
+echo "create visualization files : DONE"
+
+
+
+
+
+
+source deactivate
